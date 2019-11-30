0a1e68b8fdc776057556b7d08e1af14641504fe6
nonnywind <42970258+nonnywind@users.noreply.github.com>
2019-11-30 22:01:44 +0700

    Update view planning fitur baru input excel

--- application/views/planning/input_potensial.php
+++ application/views/planning/input_potensial.php
@@ -6,7 +6,9 @@
     <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
     <div class="row">
         <div class="col-lg">
-            <!-- excel -->
+            <button class="btn btn-sm btn-facebook mb-3 input-file-potential" type="button" data-toggle="modal" data-target="#modalInputFilePotential">
+                <i class="fas fa-plus-circle text-white mr-2"></i>
+                Input Data With Excel</button>
             
             <div class="row">
                 <div class="col-lg-4">
